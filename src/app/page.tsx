b78695a86a7090296e19168efd6e22665b4c95e0
𝑷𝒉𝒊𝒍𝒐𝒔𝒐𝒑𝒉𝒆𝒓 . <you@example.com>
2025-07-21 19:26:23 +0100

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Culture from "./components/Culture";
 import Hero from "./components/Hero";
 import AboutSection from "./components/AboutSection";
 import ShopSection from "./components/ShopSection";
-import OrderSection from "./components/Ordersection";
+import OrderSection from "./components/OrderSection";
 
 export default function Home() {
   return (
